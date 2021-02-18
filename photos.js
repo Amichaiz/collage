8131ac5d0eb416082e05da24db9adf523d28c755
Amichai Berger <76897731+Amichaiz@users.noreply.github.com>
2021-02-18 22:51:40 +0200

    Add files via upload

---
+++ photos.js
@@ -0,0 +1,21 @@
+let sub = document.getElementById("root")
+sub.addEventListener('click', addstorage)
+let a = localStorage.getItem('root')||"Not Found";
+if (a!="Not Found"){
+    change()
+}
+function addstorage() {
+    let newroot = document.getElementById("newinput")
+    localStorage.setItem('root', newroot.value);
+    console.log(newroot.value);
+    console.log(localStorage);
+    change()
+}
+function change() {
+    let photos = document.querySelectorAll("img")
+    let i=1
+    photos.forEach(e=>{
+        e.src=`${a}\\${i}.jpg`
+        i++;
+    })
+}
